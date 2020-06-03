a020db02d17880633296fea02ec45cdc6557cadb
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-03 16:55:45 +0000

    initial commit

---
+++ client.js
@@ -0,0 +1,34 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+    const conn = net.createConnection({
+        host: '135.23.222.148',
+        port: 50541
+    });
+    // interpret incoming data as text
+    conn.setEncoding('utf8');
+
+    conn.on('connect', () => {
+        conn.write('Name: Wow');
+        conn.write('Move: left');
+        setInterval(() => {
+            conn.write('Move: left');
+        }, 500)
+        // setTimeout(() => {
+        // }, 500);
+        // conn.write('Move: up');
+        // conn.write('Move: up');
+        // conn.write('Move: up');
+
+        // console.log('Successfully connected to game server');
+    })
+
+    return conn;
+}
+
+module.exports = {
+    connect: connect
+}
