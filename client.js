70802d07f950b3c209f33d0a1c6c73c3fd36377c
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-03 18:25:48 +0000

    sent movement to server done

--- client.js
+++ client.js
@@ -10,23 +10,24 @@ const connect = function() {
     });
     // interpret incoming data as text
     conn.setEncoding('utf8');
-
     conn.on('connect', () => {
         conn.write('Name: Wow');
-        conn.write('Move: left');
-        let testing = setInterval(() => {
-            conn.write('Move: left');
-        }, 500)
-
-        clearTimeout(testing);
-        // setTimeout(() => {
-        // }, 500);
-        // conn.write('Move: up');
-        // conn.write('Move: up');
-        // conn.write('Move: up');
-
-        // console.log('Successfully connected to game server');
     })
+
+    // conn.on('connect', () => {
+    //     conn.write('Name: Wow');
+    //     conn.write('Move: left');
+    //     setInterval(() => {
+    //         conn.write('Move: left');
+    //     }, 500)
+
+    //     // clearTimeout(testing);
+    //     // setTimeout(() => {
+    //     // }, 500);
+    //     // conn.write('Move: up');
+
+    //     // console.log('Successfully connected to game server');
+    // })
 
     return conn;
 }
--- input.js
+++ input.js
@@ -1,16 +1,33 @@
-const handleUserInput = userInput => {
+let connection;
+
+const handleUserInput = (userInput, conn) => {
+    const instruction = {
+        'w': 'up',
+        'a': 'left',
+        's': 'down',
+        'd': 'right'
+    }
     // Check if it is Ctrl-C
+    console.log('userInput:', userInput);
     if (userInput === '\u0003') {
         process.exit();
     }
+    else if (instruction[userInput]) {
+        conn.write(`Move: ${instruction[userInput]}`);
+
+    } else {
+        console.log('Invalid userInput');
+    }
+
 }
 
-const setupInput = () => {
+const setupInput = (conn) => {
+    connection = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding('utf8');
     stdin.resume();
-    stdin.on('data', key => handleUserInput(key));
+    stdin.on('data', key => handleUserInput(key, connection));
     return stdin;
 }
 
